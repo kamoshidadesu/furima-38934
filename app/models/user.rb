14d187f39b602571534eab1e8b429a2d8dfbbd3b
kamoshidadesu <tatsuya2630@gmail.com>
2023-03-13 21:53:46 +0900

    userモデルのバリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  # ユーザーの新規登録には、パスワードは確認用を含めて2回入力すること
+  validates :password, confirmation: true
+  # ユーザーの新規登録には、ニックネーム、メールアドレス、
+  validates :nickname, presence: true
+  validates :email, presence: true
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  validates :birth, presence: true
+  has_many :items
+  has_many :buys
 end
