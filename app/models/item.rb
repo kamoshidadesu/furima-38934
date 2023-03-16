0336fae15391c4a2a3006a2e279ef6e643803f01
kamoshidadesu <tatsuya2630@gmail.com>
2023-03-16 18:33:23 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,13 @@
 class Item < ApplicationRecord
   belongs_to :user
   #has_one :buy
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :category_id, presence: true
+  validates :condition_id, presence: true
+  validates :state_id, presence: true
+  validates :freight_id, presence: true
+  validates :shipdate_id, presence: true
+  validates :price, presence: true
 end
