9578f6997e552b35149ae01f29876c1efa036bde
kamoshidadesu <tatsuya2630@gmail.com>
2023-03-17 19:40:11 +0900

    モデルファイルの作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,6 +5,10 @@ class Item < ApplicationRecord
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
+  belongs_to :condition
+  belongs_to :state
+  belongs_to :freight
+
 
   #空の投稿を保存できないようにする
   validates :name, presence: true
@@ -18,4 +22,8 @@ class Item < ApplicationRecord
 
   #ジャンルの選択が「---」の時は保存できないようにする
   validates :category_id, numericality: { other_than: 1 message: "can't be blank" } 
+  validates :condition_id, numericality: { other_than: 1 message: "can't be blank" } 
+  validates :state_id, numericality: { other_than: 1 message: "can't be blank" } 
+  validates :freight_id, numericality: { other_than: 1 message: "can't be blank" } 
+  validates :shipdate_id, numericality: { other_than: 1 message: "can't be blank" } 
 end
