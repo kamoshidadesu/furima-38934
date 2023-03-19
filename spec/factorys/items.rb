2b3f88492b337ab9cd7bede2ea7bd6c2ed8eb6bf
kamoshidadesu <tatsuya2630@gmail.com>
2023-03-19 21:39:28 +0900

    itemのexampleの作成

--- spec/factorys/items.rb
+++ spec/factorys/items.rb
@@ -1,5 +1,16 @@
 FactoryBot.define do
   factory :item do
+
+    image{Faker::Lorem.sentence}
+    name {Faker::Name.name}
+    description{Faker::Lorem.sentence}
+    category_id{Faker::Number.between(from: 2, to: 11)}
+    condition_id{Faker::Number.between(from: 2, to: 7)}
+    state_id{Faker::Number.between(from: 2, to: 48)}
+    freight_id{Faker::Number.between(from: 2, to: 3)}
+    shipdate_id{Faker::Number.between(from: 2, to: 4)}
+    price{Faker::Number.between(from: 300, to: 9999999)}
+    association :user
     
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
 end
+
+
+  describe'出品の保存' do
+    context'出品ができる場合' do
+      it '商品名、商品の説明、カテゴリー、商品の状態、配送料の負担、発送元の地域、発送までの日数、販売価格が存在すれば出品できる' do
+      expect(@item).to be_valid
+      end
+  end
+    context '出品ができない場合' do
+      it '商品画像が空では出品できない' do
+      end
+      it '商品名が空では出品できない' do
+      end
+      it '商品の説明が空では出品できない' do
+      end
+      it 'カテゴリーが空では出品できない' do
+      end
+      it '商品の状態が空では出品ができない' do
+      end
+      it '配送料の負担が空では出品できない' do
+      end
+      it '発送元の地域が空では出品できない' do
+      end
+      it '発送までの日数が空では出品できない'do
+      end
+      it '販売価格が空では出品できない' do
+      end
+
+    end
