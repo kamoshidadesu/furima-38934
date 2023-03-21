545b55a564c38c840718a2de7d585fc9c3225bec
kamoshidadesu <tatsuya2630@gmail.com>
2023-03-21 15:50:18 +0900

    テストコードの記述修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -27,33 +27,33 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Description can't be blank")
       end
-      it 'カテゴリーが空では出品できない' do
-        @item.category = nil
+      it 'カテゴリーが---では出品できない' do
+        @item.category_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Category can't be blank")
       end
-      it '商品の状態が空では出品ができない' do
-        @item.condition = nil
+      it '商品の状態が---では出品ができない' do
+        @item.condition_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Condition can't be blank")
       end
-      it '配送料の負担が空では出品できない' do
-        @item.state = nil
+      it '配送料の負担が---では出品できない' do
+        @item.state_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("State can't be blank")
       end
-      it '発送元の地域が空では出品できない' do
-        @item.freight = nil
+      it '発送元の地域が---では出品できない' do
+        @item.freight_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Freight can't be blank")
       end
-      it '発送までの日数が空では出品できない' do
-        @item.shipdate = nil
+      it '発送までの日数が---では出品できない' do
+        @item.shipdate_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Shipdate can't be blank")
       end
       it '販売価格が空では出品できない' do
-        @item.price = ''
+        @item.price = nil
         @item.valid?
         expect(@item.errors.full_messages).to include("Price can't be blank")
       end
