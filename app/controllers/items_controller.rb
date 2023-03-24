9422e48057dd7f56815df331d5fe9b277e8926ce
kamoshidadesu <tatsuya2630@gmail.com>
2023-03-24 17:28:15 +0900

    商品の編集機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -26,6 +26,11 @@ before_action :authenticate_user!, except: [:index,  :show]
     @item = Item.find(params[:id])
   end
 
+  def update
+    item = Item.find(params[:id])
+    item.update(item_params)
+    
+  end
 
   private
 
