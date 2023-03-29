21b9a6ac846d6a00e2245f3552c7e3b4395239b8
kamoshidadesu <tatsuya2630@gmail.com>
2023-03-29 16:17:13 +0900

    order_shippingのバリデーションの追加

--- app/models/order_shipping.rb
+++ app/models/order_shipping.rb
@@ -3,12 +3,12 @@ class OrderShipping
   attr_accessor :user_id, :item_id, :zip, :state_id, :city, :address, :building_name, :phone
 
   with_options presence: true do
-    validates :zip
+    validates :zip, format: {with: /\A\d{3}[-]\d{4}\z/}
     validates :state_id,numericality: { other_than: 1, message: "can't be blank" }
     validates :city
     validates :address
     validates :building_name
-    validates :phone
+    validates :phone, format: {with: /\A\d{10,11}\z/}
     validates :user_id
     validates :item_id
 end
