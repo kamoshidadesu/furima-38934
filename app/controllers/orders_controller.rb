fe2293fa0f03db1a505010c5e784053c9d07af8e
kamoshidadesu <tatsuya2630@gmail.com>
2023-03-30 17:22:35 +0900

    soldoutの表示、ログインの有無での条件分岐

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,13 @@
 class OrdersController < ApplicationController
+  before_action :set_item, only: [:index, :create]
+  before_action :authenticate_user!
+  
   def index
-    @order_shipping = OrderShipping.new
-    @item = Item.find(params[:item_id])
+    if current_user.id == @item.user_id
+      redirect_to root_path
+    else
+      @order_shipping = OrderShipping.new
+    end
   end
 
   def new
@@ -10,8 +16,6 @@ class OrdersController < ApplicationController
 
   def create
     @order_shipping = OrderShipping.new(order_params)
-    @item = Item.find(params[:item_id])
-    
       if @order_shipping.valid?
         pay_item
         @order_shipping.save
@@ -34,5 +38,9 @@ class OrdersController < ApplicationController
           card: order_params[:token],    # カードトークン
           currency: 'jpy'                 # 通貨の種類（日本円）
         )
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
   end
 end
